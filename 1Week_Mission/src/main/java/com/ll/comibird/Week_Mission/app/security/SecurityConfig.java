25477ec1d32aef6fac8a589d990613c0efd6446c
jdi022222 <jdi022222@gmail.com>
2022-10-17 15:32:24 +0900

    Refactor : Optimize Imports

--- 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/security/SecurityConfig.java
+++ 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/security/SecurityConfig.java
@@ -7,11 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import static org.springframework.http.HttpMethod.*;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
