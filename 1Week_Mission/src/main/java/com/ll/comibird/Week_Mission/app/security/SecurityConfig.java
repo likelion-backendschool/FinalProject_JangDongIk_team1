58ff41ad614da79da8268c235783b3f71429dcd5
jdi022222 <jdi022222@gmail.com>
2022-10-18 11:08:26 +0900

    Fix : resource 디렉토리 오류 수정

--- 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/security/SecurityConfig.java
+++ 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/security/SecurityConfig.java
@@ -7,42 +7,29 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+    private final AuthenticationFailureHandler authenticationFailureHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
         http
-                .authorizeRequests(
-                        authorizeRequests -> authorizeRequests
-                                .antMatchers("/**").permitAll()
-                )
-                // 타 도메인에서 API 호출 가능
-                .cors(
-                        cors -> cors
-                                .disable()
-                )
-                // csrf 토큰 사용 X (추후 수정 필요)
-                .csrf(
-                        csrf -> csrf
-                                .disable()
-                )
                 .formLogin(
                         formLogin -> formLogin
-                                .loginPage("/member/login")
-                                // 로그인 성공 시 호출되는 URL
-                                .defaultSuccessUrl("/")
+                                .loginPage("/member/login") // GET
+                                .loginProcessingUrl("/member/login") // POST
+                                .successHandler(authenticationSuccessHandler)
+                                .failureHandler(authenticationFailureHandler)
                 )
                 .logout(
-                        logout -> logout
-                                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-                                .logoutSuccessUrl("/")
-                                .invalidateHttpSession(true)
-                )
-        ;
+                        logout -> logout.logoutUrl("/member/logout")
+                );
 
         return http.build();
     }
