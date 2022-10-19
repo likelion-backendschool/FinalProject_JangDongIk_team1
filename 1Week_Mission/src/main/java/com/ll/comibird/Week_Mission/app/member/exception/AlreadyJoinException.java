e63abe8e17eff9c396d4327204c34bef18c4820f
jdi022222 <jdi022222@gmail.com>
2022-10-19 17:54:49 +0900

    Chore : 파일 백업

--- 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/member/exception/AlreadyJoinException.java
+++
@@ -1,4 +0,0 @@
-package com.ll.comibird.Week_Mission.app.member.exception;
-
-public class AlreadyJoinException extends RuntimeException{
-}
--- 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/member/form/ModifyForm.java
+++
@@ -1,9 +0,0 @@
-package com.ll.comibird.Week_Mission.app.member.form;
-
-import lombok.Data;
-
-@Data
-public class ModifyForm {
-    private String email;
-    private String nickname;
-}
