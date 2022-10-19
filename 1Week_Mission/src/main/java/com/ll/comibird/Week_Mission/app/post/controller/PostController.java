72c9ff2622bdba089839b274c1572de80a78c59c
jdi022222 <jdi022222@gmail.com>
2022-10-19 14:39:30 +0900

    Feat : 글 작성

--- 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/post/controller/PostController.java
+++ 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/post/controller/PostController.java
@@ -1,14 +1,21 @@
 package com.ll.comibird.Week_Mission.app.post.controller;
 
 import com.ll.comibird.Week_Mission.app.post.entity.Post;
+import com.ll.comibird.Week_Mission.app.post.form.PostForm;
 import com.ll.comibird.Week_Mission.app.post.service.PostService;
+import com.ll.comibird.Week_Mission.app.security.dto.MemberContext;
+import com.ll.comibird.Week_Mission.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartRequest;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -25,5 +32,20 @@ public class PostController {
         model.addAttribute("posts", posts);
 
         return "post/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/write")
+    public String showWrite() {
+        return "post/write";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/write")
+    public String write(@AuthenticationPrincipal MemberContext memberContext, @Valid PostForm postForm) {
+
+        Post post = postService.write(memberContext.getMember(), postForm.getSubject(), postForm.getContent(), postForm.getHashTagContents());
+
+        return "redirect:/post/list?msg=" + Util.url.encode(post.getId()+"번 개시물이 작성되었습니다.");
     }
 }
--- 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/post/service/PostService.java
+++ 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/post/service/PostService.java
@@ -1,17 +1,33 @@
 package com.ll.comibird.Week_Mission.app.post.service;
 
+import com.ll.comibird.Week_Mission.app.member.entity.Member;
 import com.ll.comibird.Week_Mission.app.post.entity.Post;
 import com.ll.comibird.Week_Mission.app.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+    private final PostHashTagService postHashTagService;
     public List<Post> findAllByOrderByIdDesc() {
         return postRepository.findAllByOrderByIdDesc();
+    }
+
+    public Post write(Member member, String subject, String content, String hashTagContents) {
+        Post post = Post.builder()
+                .author(member)
+                .subject(subject)
+                .content(content)
+                .createDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+        postRepository.save(post);
+        postHashTagService.applyHashTags(post, hashTagContents);
+        return post;
     }
 }
