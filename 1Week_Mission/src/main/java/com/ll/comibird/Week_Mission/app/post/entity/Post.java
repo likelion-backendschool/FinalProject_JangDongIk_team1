4ef8f93a24edc687ae69b91a3aeda3acf6003f16
jdi022222 <jdi022222@gmail.com>
2022-10-19 16:59:58 +0900

    Feat : 글 수정

--- 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/post/entity/Post.java
+++ 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/post/entity/Post.java
@@ -7,6 +7,7 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
--- 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/post/form/PostForm.java
+++ 1Week_Mission/src/main/java/com/ll/comibird/Week_Mission/app/post/form/PostForm.java
@@ -10,5 +10,5 @@ public class PostForm {
     private String subject;
     @NotEmpty
     private String content;
-    private String hashTagContents;
+    private String postHashTagContents;
 }
