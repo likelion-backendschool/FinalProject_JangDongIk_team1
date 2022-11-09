e18c9fc68e9be182d28666dabf2787a5d3ce39ba
jdi022222 <jdi022222@gmail.com>
2022-11-09 11:30:55 +0900

    Feat : 로그인 API 생성, 테스트케이스 추가

---
+++ 4Week_Mission/src/main/java/com/ll/exam/mutBook/app/member/controller/ApiV1MemberController.java
@@ -0,0 +1,32 @@
+package com.ll.exam.mutBook.app.member.controller;
+
+import lombok.Data;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("api/v1/member")
+public class ApiV1MemberController {
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication", "JWT키");
+
+        String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
+    }
+
+    @Data
+    public static class LoginDto {
+        private String username;
+        private String password;
+    }
+}
