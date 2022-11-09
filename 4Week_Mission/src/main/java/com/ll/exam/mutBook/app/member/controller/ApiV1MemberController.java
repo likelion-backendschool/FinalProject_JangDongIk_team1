d1d21af0268f726d7e3c3cd2cbca2d1d7903f397
jdi022222 <jdi022222@gmail.com>
2022-11-09 11:37:45 +0900

    Feat : 로그인 API 입력 데이터 유효성 검사, 테스트케이스 추가

--- 4Week_Mission/src/main/java/com/ll/exam/mutBook/app/member/controller/ApiV1MemberController.java
+++ 4Week_Mission/src/main/java/com/ll/exam/mutBook/app/member/controller/ApiV1MemberController.java
@@ -1,23 +1,46 @@
 package com.ll.exam.mutBook.app.member.controller;
 
+import com.ll.exam.mutBook.app.member.entity.Member;
+import com.ll.exam.mutBook.app.member.service.MemberService;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-
 @RestController
 @RequestMapping("api/v1/member")
+@RequiredArgsConstructor
 public class ApiV1MemberController {
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authentication", "JWT키");
+
+        Member member = memberService.findByUsername(loginDto.getUsername()).orElse(null);
+
+        // 로그인 정보 유효성 검사
+        if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
+        // 회원 정보 유효성 검사
+        if (member == null) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
+        // 비밀번호 유효성 검사
+        if (passwordEncoder.matches(loginDto.getPassword(), member.getPassword()) == false) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
 
         String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
 
@@ -28,5 +51,9 @@ public class ApiV1MemberController {
     public static class LoginDto {
         private String username;
         private String password;
+
+        public boolean isNotValid() {
+            return username == null || password == null || username.trim().length() == 0 || password.trim().length() == 0;
+        }
     }
 }
